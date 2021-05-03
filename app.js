371eb0d70747a92b89a1b397cdc86b6ef2e12082
mohssine92 <mohssinelmariouh@gmail.com>
2021-05-03 11:46:41 +0200

    Servir Contenido estatico

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const express = require('express')
 const app = express()
 const port = 8080
  
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+// Servir contenido estatico , Middelware de express
+app.use( express.static('public'));
+
 
 app.get('/hi', (req, res) => {
     res.send('hi Mohssine')
 })
 
 app.get('*', (req, res) => {
-    res.send('404 | page not found ')
+    // res.send('404 | page not found ') 
+    res.sendFile(__dirname + '/public/404.html');
 })
 
 
