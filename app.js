843b8326c649cff260e4348b48aedec0795657ec
mohssine92 <mohssinelmariouh@gmail.com>
2021-05-03 15:23:06 +0200

    94 - Preparar Web server para subirlo a un hosting

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express')
 const hbs = require('hbs');
 
 
 const app = express()
-const port = 8080
+const port = process.env.PORT
  
 
 //Handelbars
