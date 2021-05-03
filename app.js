341efd7eea08d04de1833a4cbb90842a8aeb69a2
mohssine92 <mohssinelmariouh@gmail.com>
2021-05-03 12:33:33 +0200

    90 - servir un sitio web completo - Usando Express

--- app.js
+++ app.js
@@ -7,14 +7,24 @@ const port = 8080
 app.use( express.static('public'));
 
 
-app.get('/hi', (req, res) => {
-    res.send('hi Mohssine')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
 })
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
+})
+
+app.get('/elements', (req, res) => {
+    res.sendFile(__dirname + '/public/elements.html');
+})
+
+
 
 app.get('*', (req, res) => {
     // res.send('404 | page not found ') 
     res.sendFile(__dirname + '/public/404.html');
 })
+
 
 
 app.listen(port, () => {
